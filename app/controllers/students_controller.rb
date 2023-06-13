f664bf8e6302fab86098d3612d7943edefc8c794
Tina Shi <init.defaultBranch>
2023-06-14 03:01:36 +1000

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+  
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
